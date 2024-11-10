110f06583357ab2e1ea2aa53193dbe23675bba99
Teklaw7 <timothee.teyssier@cpe.fr>
2024-11-10 23:43:32 +0100

    Description stage kitware sas + skills + about

--- script.js
+++ script.js
@@ -4,21 +4,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const locations = [
-    { 
-        coords: [46.603354, 1.888334], 
-        popup: "France : Projet en vision industrielle. <a href='projetFrance.html' target='_blank'>En savoir plus</a>" 
+    {
+        coords: [46.603354, 1.888334],
+        popup: "France : Projet en vision industrielle",
+        url: "projetFrance.html" // Page de destination pour ce projet
     },
-    { 
-        coords: [35.9049, -79.0484], 
-        popup: "États-Unis : Année de césure de recherche en imagerie médicale à UNC. <a href='projetUNC.html' target='_blank'>En savoir plus</a>" 
+    {
+        coords: [35.9049, -79.0484],
+        popup: "États-Unis : Année de césure de recherche en imagerie médicale à UNC",
+        url: "projetUNC.html" // Page de destination pour ce projet
     },
 ];
 
 locations.forEach(location => {
-    L.circle(location.coords, {
+    const marker = L.circle(location.coords, {
         color: 'blue',
         fillColor: '#4CAF50',
         fillOpacity: 0.5,
         radius: 500000
-    }).addTo(map).bindPopup(location.popup);
+    }).addTo(map);
+
+    marker.bindPopup(`<a href="#" onclick="navigateToPage('${location.url}')">${location.popup}</a>`);
 });
+
+// Fonction de redirection vers la page dans le même onglet
+function navigateToPage(url) {
+    window.location.href = url; // Redirection dans le même onglet
+}
